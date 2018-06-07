41b0ec35a8674ccade130ac7f0a9533e4780c875
leornado <leornado@126.com>
2018-06-07 11:18:52 +0800

    change style

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   var $window = $(window);
   var $stateSwitch = $('#state-switch');
   var sectionTop = $('.top').outerHeight() + 20;
@@ -7,7 +7,7 @@ $(function() {
   hljs.initHighlightingOnLoad();
 
   // navigation
-  $('a[href^="#"]').on('click', function(event) {
+  $('a[href^="#"]').on('click', function (event) {
     event.preventDefault();
     var $target = $($(this).attr('href'));
 
@@ -17,10 +17,10 @@ $(function() {
   });
 
   $('input[name="download-version"]').on({
-    'init.bootstrapSwitch': function() {
+    'init.bootstrapSwitch': function () {
       $('#download-' + ($(this).is(':checked') ? '2' : '3')).hide();
     },
-    'switchChange.bootstrapSwitch': function(event, state) {
+    'switchChange.bootstrapSwitch': function (event, state) {
       $('#download-3')[state ? 'show' : 'hide']();
       $('#download-2')[state ? 'hide' : 'show']();
     }
@@ -43,115 +43,132 @@ $(function() {
     alert($stateSwitch.bootstrapSwitch('state'));
   });
 
+  $('#switch-mini').bootstrapSwitch('size', 'mini');
+  $('#switch-small').bootstrapSwitch('size', 'small');
+  $('#switch-normal').bootstrapSwitch('size', 'normal');
+  $('#switch-large').bootstrapSwitch('size', 'large');
+
+  $('#switch-on-primary').bootstrapSwitch('onColor', 'primary');
+  $('#switch-on-info').bootstrapSwitch('onColor', 'info');
+  $('#switch-on-success').bootstrapSwitch('onColor', 'success');
+  $('#switch-on-warning').bootstrapSwitch('onColor', 'warning');
+  $('#switch-on-danger').bootstrapSwitch('onColor', 'danger');
+
+  $('#switch-off-primary').bootstrapSwitch('offColor', 'primary');
+  $('#switch-off-info').bootstrapSwitch('offColor', 'info');
+  $('#switch-off-success').bootstrapSwitch('offColor', 'success');
+  $('#switch-off-warning').bootstrapSwitch('offColor', 'warning');
+  $('#switch-off-danger').bootstrapSwitch('offColor', 'danger');
+
   /*
-  // dimension
-  $('#btn-size-regular-switch').on('click', function () {
-    $('#dimension-switch').bootstrapSwitch('size', '');
-  });
-  $('#btn-size-mini-switch').on('click', function () {
-    $('#dimension-switch').bootstrapSwitch('size', 'mini');
-  });
-  $('#btn-size-small-switch').on('click', function () {
-    $('#dimension-switch').bootstrapSwitch('size', 'small');
-  });
-  $('#btn-size-large-switch').on('click', function () {
-    $('#dimension-switch').bootstrapSwitch('size', 'large');
-  });
+   // dimension
+   $('#btn-size-regular-switch').on('click', function () {
+   $('#dimension-switch').bootstrapSwitch('size', '');
+   });
+   $('#btn-size-mini-switch').on('click', function () {
+   $('#dimension-switch').bootstrapSwitch('size', 'mini');
+   });
+   $('#btn-size-small-switch').on('click', function () {
+   $('#dimension-switch').bootstrapSwitch('size', 'small');
+   });
+   $('#btn-size-large-switch').on('click', function () {
+   $('#dimension-switch').bootstrapSwitch('size', 'large');
+   });
 
 
 
-  // destroy
-  $('#btn-destroy-switch').on('click', function () {
-    $('#destroy-switch').bootstrapSwitch('destroy');
-    $(this).remove();
-  });
-  // CREATE
-  $('#btn-create').on('click', function () {
-    $('#create-switch').bootstrapSwitch();
-    $(this).remove();
-  });
+   // destroy
+   $('#btn-destroy-switch').on('click', function () {
+   $('#destroy-switch').bootstrapSwitch('destroy');
+   $(this).remove();
+   });
+   // CREATE
+   $('#btn-create').on('click', function () {
+   $('#create-switch').bootstrapSwitch();
+   $(this).remove();
+   });
 
-  // activation
-  var $disable = $('#disable-switch');
-  $('#btn-disable-is').on('click', function () {
-    alert($disable.bootstrapSwitch('disabled'));
-  });
-  $('#btn-disable-toggle').on('click', function () {
-    $disable.bootstrapSwitch('toggleDisabled');
-  });
-  $('#btn-disable-set').on('click', function () {
-    $disable.bootstrapSwitch('disabled', true);
-  });
-  $('#btn-disable-remove').on('click', function () {
-    $disable.bootstrapSwitch('disabled', false);
-  });
+   // activation
+   var $disable = $('#disable-switch');
+   $('#btn-disable-is').on('click', function () {
+   alert($disable.bootstrapSwitch('disabled'));
+   });
+   $('#btn-disable-toggle').on('click', function () {
+   $disable.bootstrapSwitch('toggleDisabled');
+   });
+   $('#btn-disable-set').on('click', function () {
+   $disable.bootstrapSwitch('disabled', true);
+   });
+   $('#btn-disable-remove').on('click', function () {
+   $disable.bootstrapSwitch('disabled', false);
+   });
 
-  // readonly
-  var $readonly = $('#readonly-switch');
-  $('#btn-readonly-is').on('click', function () {
-    alert($readonly.bootstrapSwitch('readonly'));
-  });
-  $('#btn-readonly-toggle').on('click', function () {
-    $readonly.bootstrapSwitch('toggleReadonly');
-  });
-  $('#btn-readonly-set').on('click', function () {
-    $readonly.bootstrapSwitch('readonly', true);
-  });
-  $('#btn-readonly-remove').on('click', function () {
-    $readonly.bootstrapSwitch('readonly', false);
-  });
+   // readonly
+   var $readonly = $('#readonly-switch');
+   $('#btn-readonly-is').on('click', function () {
+   alert($readonly.bootstrapSwitch('readonly'));
+   });
+   $('#btn-readonly-toggle').on('click', function () {
+   $readonly.bootstrapSwitch('toggleReadonly');
+   });
+   $('#btn-readonly-set').on('click', function () {
+   $readonly.bootstrapSwitch('readonly', true);
+   });
+   $('#btn-readonly-remove').on('click', function () {
+   $readonly.bootstrapSwitch('readonly', false);
+   });
 
-  // label
-  $('#btn-label-on-switch').on('click', function() {
-    $('#label-switch').bootstrapSwitch('onText', 'I');
-  });
-  $('#btn-label-off-switch').on('click', function() {
-    $('#label-switch').bootstrapSwitch('offText', 'O');
-  });
+   // label
+   $('#btn-label-on-switch').on('click', function() {
+   $('#label-switch').bootstrapSwitch('onText', 'I');
+   });
+   $('#btn-label-off-switch').on('click', function() {
+   $('#label-switch').bootstrapSwitch('offText', 'O');
+   });
 
-  $('#label-toggle-switch').on('click', function(e, data) {
-    $('.label-toggle-switch').bootstrapSwitch('toggleState');
-  });
-  $('.label-toggle-switch').on('switch-change', function(e, data) {
-    alert(data.value);
-  });
+   $('#label-toggle-switch').on('click', function(e, data) {
+   $('.label-toggle-switch').bootstrapSwitch('toggleState');
+   });
+   $('.label-toggle-switch').on('switch-change', function(e, data) {
+   alert(data.value);
+   });
 
-  // event handler
-  $('#events-switch').on({
-    init: function() {
-      console.log('Initialized!');
-    },
-    switchChange: function ($element, data) {
-      var $element = $(data.el),
-        value = data.value;
+   // event handler
+   $('#events-switch').on({
+   init: function() {
+   console.log('Initialized!');
+   },
+   switchChange: function ($element, data) {
+   var $element = $(data.el),
+   value = data.value;
 
-      console.log(e, $element, value);
-    }
-  }).bootstrapSwitch();
+   console.log(e, $element, value);
+   }
+   }).bootstrapSwitch();
 
-  // color
-  $('#btn-color-on-switch').on('click', function() {
-    $('#change-color-switch').bootstrapSwitch('onColor', 'success');
-  });
-  $('#btn-color-off-switch').on('click', function() {
-    $('#change-color-switch').bootstrapSwitch('offColor', 'danger');
-  });
+   // color
+   $('#btn-color-on-switch').on('click', function() {
+   $('#change-color-switch').bootstrapSwitch('onColor', 'success');
+   });
+   $('#btn-color-off-switch').on('click', function() {
+   $('#change-color-switch').bootstrapSwitch('offColor', 'danger');
+   });
 
-  // animation
-  $('#btn-animate-switch').on('click', function() {
-    $('#animated-switch').bootstrapSwitch('animate', true);
-  });
-  $('#btn-dont-animate-switch').on('click', function() {
-    $('#animated-switch').bootstrapSwitch('animate', false);
-  });
+   // animation
+   $('#btn-animate-switch').on('click', function() {
+   $('#animated-switch').bootstrapSwitch('animate', true);
+   });
+   $('#btn-dont-animate-switch').on('click', function() {
+   $('#animated-switch').bootstrapSwitch('animate', false);
+   });
 
-  // radio
-  $('.radio1').on('switch-change', function () {
-    $('.radio1').bootstrapSwitch('toggleRadioState');
-  });
-  $('.radio2').on('switch-change', function () {
-    console.log("ok");
-    $('.radio2').bootstrapSwitch('toggleRadioState', true);
-  });
-  */
+   // radio
+   $('.radio1').on('switch-change', function () {
+   $('.radio1').bootstrapSwitch('toggleRadioState');
+   });
+   $('.radio2').on('switch-change', function () {
+   console.log("ok");
+   $('.radio2').bootstrapSwitch('toggleRadioState', true);
+   });
+   */
 });
